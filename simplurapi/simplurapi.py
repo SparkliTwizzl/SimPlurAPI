0d066b18706d5ad99e6fb76344bdea7f5b7691cd
SparkliTwizzl <github@sparklitwizzl.com>
2024-09-10 07:32:09 -0600

    Initial package setup
    [Gwen Sable (she/her)]

---
+++ simplurapi/simplurapi.py
@@ -0,0 +1,176 @@
+# authentication
+#     http
+#     socket
+# analytics
+#     GET
+#         get analytics
+# automated timers
+#     GET
+#         get automated timer
+#         get all automated timers
+#     POST
+#         add automated timer
+#     PATCH
+#         update automated timer
+#     DEL
+#         delete automated timer
+# board messages
+#     GET
+#         get board message
+#         get all board messages for member
+#         get all unread board messages
+#     POST
+#         write board message
+#     PATCH
+#         update board message
+#     DEL
+#         delete board message
+# chat messages
+#     GET
+#         get chat message
+#         get all chat messages for channel
+#     POST
+#         add chat message
+#     PATCH
+#         update chat message
+#     DEL
+#         delete chat message
+# chat categories
+#     GET
+#         get chat category
+#         get all chat categories
+#     POST
+#         add chat category
+#     PATCH
+#         update chat category
+#     DEL
+#         delete chat category
+# chat channels
+#     GET
+#         get chat channel
+#         get all chat channels
+#     POST
+#         add chat channel
+#     PATCH
+#         update chat channel
+#     DEL
+#         delete chat channel
+# comments
+#     GET
+#         get comment
+#         get all comments
+#     POST
+#         add comment
+#     PATCH
+#         update comment
+#     DEL
+#         delete comment
+# custom fronts
+#     GET
+#         get custom front
+#         get all custom fronts
+#     POST
+#         add custom front
+#     PATCH
+#         update custom front
+#     DEL
+#         delete custom front
+# friends
+#     GET
+#         get friends
+#         get recieved friend requests
+#         get sent friend requests
+#         get current fronters for friend
+#         get current fronters for all friends
+#         get friend settings for user
+#     POST
+#         add friend
+#         respond to friend request
+#     PATCH
+#         update friend settings
+#     DEL
+#         delete friend request
+#         remove friend
+# front history
+#     GET
+#         get current fronters
+#         get front history entry
+#         get all front history entries
+#         get all front history entries for member
+#     POST
+#         add front history entry
+#     PATCH
+#         update front history entry
+#     DEL
+#         delete front history entry
+# groups
+#     GET
+#         get group
+#         get all groups
+#     POST
+#         add group
+#     PATCH
+#         update group
+#     DEL
+#         delete group
+# members
+#     GET
+#         get member
+#         get all members
+#     POST
+#         add member
+#     PATCH
+#         update member
+#     DEL
+#         delete member
+# notes
+#     GET
+#         get note
+#         get all notes
+#     POST
+#         add note
+#     PATCH
+#         update note
+#     DEL
+#         delete note
+# polls
+#     GET
+#         get poll
+#         get all polls
+#     POST
+#         add poll
+#     PATCH
+#         update poll
+#     DEL
+#         delete poll
+# pluralkit
+#     GET
+#         sync member from pluralkit
+#         sync member to pluralkit
+#     POST
+#     PATCH
+#         sync all members from pluralkit
+#         sync all members to pluralkit
+#     DEL
+# repeated timers
+#     GET
+#         get repeated timer
+#         get all repeated timers
+#     POST
+#         add repeated timer
+#     PATCH
+#         update repeated timer
+#     DEL
+#         delete repeated timer
+# users
+#     GET
+#         get user id for auth token
+#         get user
+#         get all reports
+#     POST
+#         generate report
+#     PATCH
+#         update user
+#         set username
+#     DEL
+#         delete report
