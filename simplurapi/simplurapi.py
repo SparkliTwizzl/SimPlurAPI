6bc1b7d052394482d210b140291007bff447cea0
SparkliTwizzl <github@sparklitwizzl.com>
2024-09-11 07:00:28 -0600

    Adjustments to interface draft
    [Jay Collins (any˃they)]

--- simplurapi/simplurapi.py
+++ simplurapi/simplurapi.py
@@ -1,16 +1,23 @@
 __all__ = ['SimPlurAPI']
+__version__ = '0.1.0'
 
 
 class SimPlurAPI:
-    _APIbaseURL:str
-    _APIversion:int
+    _apiUrlHttp:str = "https://api.apparyllis.com/v#/"
+    _apiUrlSocket:str = "wss://api.apparyllis.com/v#/socket"
+    _apiVersion:int = 1
     _authToken:str
+    _userId:str
+
+    _devApiUrlHttp:str = "https://devapi.apparyllis.com/v#/"
+    _devApiUrlSocket:str = "wss://devapi.apparyllis.com/v#/socket"
+    _devApiVersion:int = 1
     _devAuthToken:str
-    _devUserID:str
+    _devUserId:str
+
     _isSocketConnectionAlive:bool = False
     _useDevelopmentMode:bool = True
     _useSocketConnection:bool = False
-    _userID:str
 
 
     def __init__( self ):
@@ -18,21 +25,33 @@ class SimPlurAPI:
 
 
     def use_development_mode( self ):
+        self._useDevelopmentMode = True
         raise NotImplemented
 
     def use_production_mode( self ):
+        self._useDevelopmentMode = False
         raise NotImplemented
 
     def use_http_connection( self ):
+        self._useSocketConnection = False
         raise NotImplemented
 
     def use_socket_connection( self ):
+        self._useSocketConnection = True
         raise NotImplemented
 
     def open_socket_connection( self ):
+        if not self._useSocketConnection:
+            raise ConnectionError( 'Socket connection is not enabled.' )
+        if self._isSocketConnectionAlive:
+            raise ConnectionRefusedError( 'Socket connection is already open.' )
         raise NotImplemented
 
     def close_socket_connection( self ):
+        if not self._useSocketConnection:
+            raise ConnectionError( 'Socket connection is not enabled.' )
+        if not self._isSocketConnectionAlive:
+            raise ConnectionResetError( 'Socket connection is not open.' )
         raise NotImplemented
 
 
@@ -40,118 +59,118 @@ class SimPlurAPI:
         raise NotImplemented
 
 
-    def get_automated_timer( self, timerID:str ):
+    def get_automated_timer( self, timerId:str ):
         raise NotImplemented
 
-    def get_all_automated_timers( self, systemID:str ):
+    def get_all_automated_timers( self, systemId:str ):
         raise NotImplemented
 
-    def add_automated_timer( self, timerID:str, name:str, message:str, type:int, delayInHours:int ):
+    def add_automated_timer( self, timerId:str, name:str, message:str, type:int, delayInHours:int ):
         raise NotImplemented
 
-    def update_automated_timer( self, timerID:str, name:str, message:str, type:int, delayInHours:int ):
+    def update_automated_timer( self, timerId:str, name:str, message:str, type:int, delayInHours:int ):
         raise NotImplemented
 
-    def delete_automated_timer( self, docID:str ):
+    def delete_automated_timer( self, timerId:str ):
         raise NotImplemented
 
 
-    def get_board_message( self, messageID:str ):
+    def get_board_message( self, messageId:str ):
         raise NotImplemented
 
-    def get_all_board_messages_for_member( self, memberID:str ):
+    def get_all_board_messages_for_member( self, memberId:str ):
         raise NotImplemented
 
     def get_all_unread_board_messages( self ):
         raise NotImplemented
 
-    def add_board_message( self, ID, title:str, message:str, writtenBy:str, writtenFor:str, writtenAt:int, read:bool, supportMarkdown:bool ):
+    def add_board_message( self, id, title:str, message:str, writtenBy:str, writtenFor:str, writtenAt:int, read:bool, supportMarkdown:bool ):
         raise NotImplemented
 
-    def update_board_message( self, ID, read:bool ):
+    def update_board_message( self, id, read:bool ):
         raise NotImplemented
 
-    def delete_board_message( self, ID ):
-        raise NotImplemented
-
-
-    def get_chat_message( self, ID ):
-        raise NotImplemented
-
-    def get_all_chat_messages_for_channel( self, channelID:str ):
-        raise NotImplemented
-
-    def add_chat_message( self, ID, message:str, channelID:str, writerID:str, writtenAt:int, replyToID:str ):
-        raise NotImplemented
-
-    def update_chat_message( self, ID, message:str, updatedAt:int ):
-        raise NotImplemented
-
-    def delete_chat_message( self, ID ):
+    def delete_board_message( self, id ):
         raise NotImplemented
 
 
-    def get_chat_category( self, ID ):
+    def get_chat_message( self, id ):
         raise NotImplemented
 
-    def get_all_chat_categories( self, systemID:str ):
+    def get_all_chat_messages_for_channel( self, channelId:str ):
         raise NotImplemented
 
-    def add_chat_category( self, ID, name:str, description:str, channelIDs:list ):
+    def add_chat_message( self, id, message:str, channelId:str, writerId:str, writtenAt:int, replyToId:str ):
         raise NotImplemented
 
-    def update_chat_category( self, ID, name:str, description:str, channelIDs:list ):
+    def update_chat_message( self, id, message:str, updatedAt:int ):
         raise NotImplemented
 
-    def delete_chat_category( self, ID ):
-        raise NotImplemented
-
-
-    def get_chat_channel( self, ID ):
-        raise NotImplemented
-
-    def get_all_chat_channels( self, systemID:str ):
-        raise NotImplemented
-
-    def add_chat_channel( self, ID, name:str, description:str ):
-        raise NotImplemented
-
-    def update_chat_channel( self, ID, name:str, description:str ):
-        raise NotImplemented
-
-    def delete_chat_channel( self, ID ):
+    def delete_chat_message( self, id ):
         raise NotImplemented
 
 
-    def get_comment( self, systemID:str, commentID:str ):
+    def get_chat_category( self, id ):
         raise NotImplemented
 
-    def get_all_comments( self, commentID:str, type:str ):
+    def get_all_chat_categories( self, systemId:str ):
         raise NotImplemented
 
-    def add_comment( self, commentID:str, time:int, text:str, supportMarkdown:bool, parentDocID:str, collectionID:str ):
+    def add_chat_category( self, id, name:str, description:str, channelIds:list ):
         raise NotImplemented
 
-    def update_comment( self, commentID:str, text:str, supportMarkdown:bool ):
+    def update_chat_category( self, id, name:str, description:str, channelIds:list ):
         raise NotImplemented
 
-    def delete_comment( self, commentID:str ):
+    def delete_chat_category( self, id ):
         raise NotImplemented
 
 
-    def get_custom_front( self, frontID:str, systemID:str ):
+    def get_chat_channel( self, id ):
         raise NotImplemented
 
-    def get_all_custom_fronts( self, systemID:str ):
+    def get_all_chat_channels( self, systemId:str ):
         raise NotImplemented
 
-    def add_custom_front( self, frontID:str, name:str, description:str, avatarURL:str, color:str, private:bool, preventTrusted:bool, supportDescMarkdown:bool ):
+    def add_chat_channel( self, id, name:str, description:str ):
         raise NotImplemented
 
-    def update_custom_front( self, frontID:str, name:str, description:str, avatarURL:str, color:str, private:bool, preventTrusted:bool, supportDescMarkdown:bool ):
+    def update_chat_channel( self, id, name:str, description:str ):
         raise NotImplemented
 
-    def delete_custom_front( self, frontID:str ):
+    def delete_chat_channel( self, id ):
+        raise NotImplemented
+
+
+    def get_comment( self, systemId:str, commentId:str ):
+        raise NotImplemented
+
+    def get_all_comments( self, commentId:str, type:str ):
+        raise NotImplemented
+
+    def add_comment( self, commentId:str, time:int, text:str, supportMarkdown:bool, parentDocId:str, collectionId:str ):
+        raise NotImplemented
+
+    def update_comment( self, commentId:str, text:str, supportMarkdown:bool ):
+        raise NotImplemented
+
+    def delete_comment( self, commentId:str ):
+        raise NotImplemented
+
+
+    def get_custom_front( self, frontId:str, systemId:str ):
+        raise NotImplemented
+
+    def get_all_custom_fronts( self, systemId:str ):
+        raise NotImplemented
+
+    def add_custom_front( self, frontId:str, name:str, description:str, avatarURL:str, color:str, private:bool, preventTrusted:bool, supportDescMarkdown:bool ):
+        raise NotImplemented
+
+    def update_custom_front( self, frontId:str, name:str, description:str, avatarURL:str, color:str, private:bool, preventTrusted:bool, supportDescMarkdown:bool ):
+        raise NotImplemented
+
+    def delete_custom_front( self, frontId:str ):
         raise NotImplemented
 
 
@@ -164,13 +183,13 @@ class SimPlurAPI:
     def get_all_sent_friend_requests( self ):
         raise NotImplemented
 
-    def get_current_fronters_for_friend( self, userID:str ):
+    def get_current_fronters_for_friend( self, userId:str ):
         raise NotImplemented
 
     def get_current_fronters_for_all_friends( self ):
         raise NotImplemented
 
-    def get_friend_settings_for_user( self, userID:str, friendID:str ):
+    def get_friend_settings_for_user( self, userId:str, friendId:str ):
         raise NotImplemented
 
     def add_friend( self, user:str ):
@@ -192,94 +211,94 @@ class SimPlurAPI:
     def get_all_current_fronters( self ):
         raise NotImplemented
 
-    def get_front_history_entry( self, systemID:str, entryID:str ):
+    def get_front_history_entry( self, systemId:str, entryId:str ):
         raise NotImplemented
 
-    def get_front_history_entries_for_timespan( self, systemID:str, startTime:int, endTime:int ):
+    def get_front_history_entries_for_timespan( self, systemId:str, startTime:int, endTime:int ):
         raise NotImplemented
 
-    def get_front_history_entries_for_member( self, memberID:str, startTime:int, endTime:int ):
+    def get_front_history_entries_for_member( self, memberId:str, startTime:int, endTime:int ):
         raise NotImplemented
 
-    def add_front_history_entry( self, entryID:str, customStatus:str, custom:bool, live:bool, startTime:int, endTime:int, fronterID:str ):
+    def add_front_history_entry( self, entryId:str, customStatus:str, custom:bool, live:bool, startTime:int, endTime:int, fronterId:str ):
         raise NotImplemented
 
-    def update_front_history_entry( self, entryID:str, customStatus:str, custom:bool, live:bool, startTime:int, endTime:int, fronterID:str ):
+    def update_front_history_entry( self, entryId:str, customStatus:str, custom:bool, live:bool, startTime:int, endTime:int, fronterId:str ):
         raise NotImplemented
 
-    def delete_front_history_entry( self, entryID:str ):
-        raise NotImplemented
-
-
-
-    def get_group( self, systemID:str, groupID:str ):
-        raise NotImplemented
-
-    def get_all_groups( self, systemID:str ):
-        raise NotImplemented
-
-    def add_group( self, groupID:str, name:str, description:str, color:str, private:bool, preventTrusted:bool, supportDescMarkdown:bool, emoji:str, parentID:str, members:list ):
-        raise NotImplemented
-
-    def update_group( self, groupID:str, name:str, description:str, color:str, private:bool, preventTrusted:bool, supportDescMarkdown:bool, emoji:str, parentID:str, members:list ):
-        raise NotImplemented
-
-    def delete_group( self, groupID:str ):
+    def delete_front_history_entry( self, entryId:str ):
         raise NotImplemented
 
 
-    def get_member( self, systemID:str, memberID: str ):
+
+    def get_group( self, systemId:str, groupId:str ):
         raise NotImplemented
 
-    def get_all_members( self, systemID:str ):
+    def get_all_groups( self, systemId:str ):
         raise NotImplemented
 
-    def add_member( self, memberID:str, name:str, description:str, color:str, pronouns:str, pluralkitID:str, avatarURL:str, private:bool, preventTrusted:bool, supportDescMarkdown:bool, preventFrontNotifs:bool, info ):
+    def add_group( self, groupId:str, name:str, description:str, color:str, private:bool, preventTrusted:bool, supportDescMarkdown:bool, emoji:str, parentId:str, members:list ):
         raise NotImplemented
 
-    def update_member( self, memberID:str, name:str, description:str, color:str, pronouns:str, pluralkitID:str, avatarURL:str, private:bool, preventTrusted:bool, supportDescMarkdown:bool, preventFrontNotifs:bool, info ):
+    def update_group( self, groupId:str, name:str, description:str, color:str, private:bool, preventTrusted:bool, supportDescMarkdown:bool, emoji:str, parentId:str, members:list ):
         raise NotImplemented
 
-    def delete_member( self, memberID:str ):
-        raise NotImplemented
-
-
-    def get_note( self, systemID:str, noteID:str ):
-        raise NotImplemented
-
-    def get_all_notes( self, systemID:str ):
-        raise NotImplemented
-
-    def add_note( self, noteID:str, title:str, text:str, color:str, memberID:str, timestamp:int, supportDescMarkdown:bool ):
-        raise NotImplemented
-
-    def update_note( self, noteID:str, title:str, text:str, color:str, memberID:str, timestamp:int, supportDescMarkdown:bool ):
-        raise NotImplemented
-
-    def delete_note( self, noteID:str ):
+    def delete_group( self, groupId:str ):
         raise NotImplemented
 
 
-    def get_poll( self, systemID:str, pollID:str ):
+    def get_member( self, systemId:str, memberId: str ):
         raise NotImplemented
 
-    def get_all_polls( self, systemID:str ):
+    def get_all_members( self, systemId:str ):
         raise NotImplemented
 
-    def add_poll( self, pollID:str, title:str, desc:str, allowAbstain:bool, allowVeto:bool, custom:bool, endTime:int, supportDescMarkdown:bool, options, votes ):
+    def add_member( self, memberId:str, name:str, description:str, color:str, pronouns:str, pluralkitId:str, avatarURL:str, private:bool, preventTrusted:bool, supportDescMarkdown:bool, preventFrontNotifs:bool, info ):
         raise NotImplemented
 
-    def update_poll( self, pollID:str, title:str, desc:str, allowAbstain:bool, allowVeto:bool, custom:bool, endTime:int, supportDescMarkdown:bool, options, votes ):
+    def update_member( self, memberId:str, name:str, description:str, color:str, pronouns:str, pluralkitId:str, avatarURL:str, private:bool, preventTrusted:bool, supportDescMarkdown:bool, preventFrontNotifs:bool, info ):
         raise NotImplemented
 
-    def delete_poll( self, pollID:str ):
+    def delete_member( self, memberId:str ):
         raise NotImplemented
 
 
-    def sync_member_from_pluralkit( self, memberID:str ):
+    def get_note( self, systemId:str, noteId:str ):
         raise NotImplemented
 
-    def sync_member_to_pluralkit( self, memberID:str ):
+    def get_all_notes( self, systemId:str ):
+        raise NotImplemented
+
+    def add_note( self, noteId:str, title:str, text:str, color:str, memberId:str, timestamp:int, supportDescMarkdown:bool ):
+        raise NotImplemented
+
+    def update_note( self, noteId:str, title:str, text:str, color:str, memberId:str, timestamp:int, supportDescMarkdown:bool ):
+        raise NotImplemented
+
+    def delete_note( self, noteId:str ):
+        raise NotImplemented
+
+
+    def get_poll( self, systemId:str, pollId:str ):
+        raise NotImplemented
+
+    def get_all_polls( self, systemId:str ):
+        raise NotImplemented
+
+    def add_poll( self, pollId:str, title:str, desc:str, allowAbstain:bool, allowVeto:bool, custom:bool, endTime:int, supportDescMarkdown:bool, options, votes ):
+        raise NotImplemented
+
+    def update_poll( self, pollId:str, title:str, desc:str, allowAbstain:bool, allowVeto:bool, custom:bool, endTime:int, supportDescMarkdown:bool, options, votes ):
+        raise NotImplemented
+
+    def delete_poll( self, pollId:str ):
+        raise NotImplemented
+
+
+    def sync_member_from_pluralkit( self, memberId:str ):
+        raise NotImplemented
+
+    def sync_member_to_pluralkit( self, memberId:str ):
         raise NotImplemented
 
     def sync_all_members_from_pluralkit( self ):
@@ -289,26 +308,26 @@ class SimPlurAPI:
         raise NotImplemented
 
 
-    def get_repeated_timer( self, systemID:str, timerID:str ):
+    def get_repeated_timer( self, systemId:str, timerId:str ):
         raise NotImplemented
 
-    def get_all_repeated_timers( self, systemID:str ):
+    def get_all_repeated_timers( self, systemId:str ):
         raise NotImplemented
 
-    def add_repeated_timer( self, timerID:str, title:str, text:str, dayInterval:int, time, startTime ):
+    def add_repeated_timer( self, timerId:str, title:str, text:str, dayInterval:int, time, startTime ):
         raise NotImplemented
 
-    def update_repeated_timer( self, timerID:str, title:str, text:str, dayInterval:int, time, startTime ):
+    def update_repeated_timer( self, timerId:str, title:str, text:str, dayInterval:int, time, startTime ):
         raise NotImplemented
 
-    def delete_repeated_timer( self, timerID:str ):
+    def delete_repeated_timer( self, timerId:str ):
         raise NotImplemented
 
 
     def get_user_id_for_auth_token( self ):
         raise NotImplemented
 
-    def get_user( self, userID:str ):
+    def get_user( self, userId:str ):
         raise NotImplemented
 
     def get_all_reports( self ):
@@ -317,11 +336,11 @@ class SimPlurAPI:
     def generate_report( self ):
         raise NotImplemented
 
-    def update_user( self, userID:str ):
+    def update_user( self, userId:str ):
         raise NotImplemented
 
-    def set_username( self, userID:str, username:str ):
+    def set_username( self, userId:str, username:str ):
         raise NotImplemented
 
-    def delete_report( self, reportID:str ):
+    def delete_report( self, reportId:str ):
         raise NotImplemented
