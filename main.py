b7b585443504fd2f391ff366b090587f7619e741
SparkliTwizzl <github@sparklitwizzl.com>
2024-09-14 05:42:30 -0600

    Temporary test file
    [Petra Sparrow (xey/xem)]

---
+++ main.py
@@ -0,0 +1,23 @@
+import datetime
+import logging
+import os
+import requests
+from simplurapi.simplurapi import SimPlurAPI
+
+
+if __name__ == "__main__":
+    logDir = 'test/log'
+    os.makedirs( logDir, exist_ok=True )
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    logFile = f'{logDir}/{timestamp}.log'
+    logging.basicConfig(
+            level=logging.DEBUG,
+            format='%(asctime)s.%(msecs)03d - %(name)30s - %(levelname)8s - %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S',
+            handlers=[
+                logging.StreamHandler(),
+                logging.FileHandler( logFile )])
+    api = SimPlurAPI( requests )
+    api.config.set_to_development_mode()
+    analytics = api.get_analytics()
+    print( analytics )
