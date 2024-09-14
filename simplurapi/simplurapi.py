0b66fc82ce653ae12a070f0d404c6c32c727f5d1
SparkliTwizzl <github@sparklitwizzl.com>
2024-09-14 05:54:43 -0600

    Minor refactor of variables
    [Petra Sparrow (xey/xem)]

--- simplurapi/simplurapi.py
+++ simplurapi/simplurapi.py
@@ -8,6 +8,7 @@ import logging
 import os
 import requests
 from dotenv import load_dotenv
+from typing import final
 
 
 _moduleName:str = 'SimPlurAPI'
@@ -67,28 +68,26 @@ class SimPlurAPI:
 
     config:Config
 
-    _apiUrlHttp:str
-    _apiUrlHttpDefault:str = "https://api.apparyllis.com/v#/"
-    _apiUrlSocket:str
-    _apiUrlSocketDefault:str = "wss://api.apparyllis.com/v#/socket"
-    _apiVersion:int
-    _apiVersionDefault:int = 1
-    _authToken:str
-    _userId:str
+    _API_URL_HTTP_DEFAULT:final[ str ] = "https://api.apparyllis.com/v#/"
+    _API_URL_SOCKET_DEFAULT:final[ str ] = "wss://api.apparyllis.com/v#/socket"
+    _API_VERSION_DEFAULT:final[ int ] = 1
+    _DEV_API_URL_HTTP_DEFAULT:final[ str ] = "https://devapi.apparyllis.com/v#/"
+    _DEV_API_URL_SOCKET_DEFAULT:final[ str ] = "wss://devapi.apparyllis.com/v#/socket"
+    _DEV_API_VERSION_DEFAULT:final[ int ] = 1
 
+    _apiUrlHttp:str
+    _apiUrlSocket:str
+    _apiVersion:int
+    _authToken:str
     _devApiUrlHttp:str
-    _devApiUrlHttpDefault:str = "https://devapi.apparyllis.com/v#/"
     _devApiUrlSocket:str
-    _devApiUrlSocketDefault:str = "wss://devapi.apparyllis.com/v#/socket"
     _devApiVersion:int
-    _devApiVersionDefault:int = 1
     _devAuthToken:str
     _devUserId:str
-
     _isSocketConnectionAlive:bool = False
-
     _logger:logging
     _requests:requests
+    _userId:str
 
 
     def __init__( self, requestProvider = requests ):
@@ -518,15 +517,15 @@ class SimPlurAPI:
         except Exception:
             raise FileNotFoundError( '.env file was not found.' )
 
-        self._apiUrlHttp = os.getenv( 'api_url_http', self._apiUrlHttpDefault )
-        self._apiUrlSocket = os.getenv( 'api_url_socket', self._apiUrlSocketDefault )
-        self._apiVersion = os.getenv( 'api_version', self._apiVersionDefault )
+        self._apiUrlHttp = os.getenv( 'api_url_http', self._API_URL_HTTP_DEFAULT )
+        self._apiUrlSocket = os.getenv( 'api_url_socket', self._API_URL_SOCKET_DEFAULT )
+        self._apiVersion = os.getenv( 'api_version', self._API_VERSION_DEFAULT )
         self._authToken = os.getenv( 'auth_token' )
         self._userId = os.getenv( 'user_id' )
 
-        self._devApiUrlHttp = os.getenv( 'dev_api_url_http', self._devApiUrlHttpDefault )
-        self._devApiUrlSocket = os.getenv( 'dev_api_url_socket', self._devApiUrlSocketDefault )
-        self._devApiVersion = os.getenv( 'dev_api_version', self._devApiVersionDefault )
+        self._devApiUrlHttp = os.getenv( 'dev_api_url_http', self._DEV_API_URL_HTTP_DEFAULT )
+        self._devApiUrlSocket = os.getenv( 'dev_api_url_socket', self._DEV_API_URL_SOCKET_DEFAULT )
+        self._devApiVersion = os.getenv( 'dev_api_version', self._DEV_API_VERSION_DEFAULT )
         self._devAuthToken = os.getenv( 'dev_auth_token' )
         self._devUserId = os.getenv( 'dev_user_id' )
 
